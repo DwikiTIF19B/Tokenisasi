1173c62b2a0be02b7bc3215969168368925a60e6
DwikiTIF19B <vivanavy379@gmail.com>
2021-05-06 11:13:28 +0700

    Project Tokenisasi

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Tokenisasi</title>
+</head>
+<body>
+<h4>Nama : M. Dwiki Yulianto</h4>
+<h4>NIM : 23419018</h4>
+<h4>Kelas : Teknik Informatika 2019 B</h4>
+<h4>Mata Kuliah : Teori Bahasa Formal & Otomata</h4>
+<h2>Tokenisasi (Pemecahan Kata)</h2>
+<b>Masukkan kalimat yang ingin dipecah:</b>
+<form method="POST" action="proses.php">
+<p><textarea name="text" id="text" cols="100" rows="10" tabindex="4"></textarea></p>
+<p>Tampilkan <input  type="text" name="banyak" style="width:30px" /> frekuensi teratas</p>
+<input type="submit" value="submit" />
+</form>
+<p><b>Contoh kalimat: </b><br><br><b>Saya Bangga jadi warga nahdlatul ulama</b>
+</body>
+</html>
